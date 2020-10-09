0aa95f5e33b243db5e38f5bf4a349e22c6a21241
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-09 10:58:44 -0500

    local state updatring for submit function

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -3,26 +3,31 @@ import {connect} from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import InfoItem from '../InfoItem/InfoItem';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
-// const InfoPage = () => (
-  
-//   <div>
-//     <p>
-//       Shelf Page
-//     </p>
-//   </div>
-// );
-
-// If you needed to add local state or other things,
-// you can make it a class component like:
 
 
 class InfoPage extends React.Component {
-  
+
+  state = {
+      description: '',
+      image: '',
+      user_id: this.props.store.user.id
+  };
+  onChangeHandler = ( event, propertyName) => {
+    console.log('we are changing', propertyName);
+    this.setState({
+     
+      ...this.state,
+      [propertyName]: event.target.value
+      
+    })
+    
+  }
+  addItem = (event) => {
+    event.preventDefault();
+    console.log('Current State:', this.state);
+    console.log('Clicked addItem!');
+  }
+
   getShelf = () => {
     this.props.dispatch({
       type: 'FETCH_SHELF_ITEM'
@@ -34,9 +39,30 @@ class InfoPage extends React.Component {
 }
 
   render() {
+
+    console.log('Updated state:', this.state);
     return (
+
       <div>
         <p>Info Page</p>
+        
+         <form onSubmit={this.addItem}> 
+          <input
+            placeholder='Item Description'
+            type='text'
+            value={this.state.description}
+            onChange={(event) => this.onChangeHandler(event, 'description')}
+            ></input>{'   '}
+          <br />
+          <input
+            placeholder='Image URL'
+            type='text'
+            value={this.state.image}
+            onChange={(event) => this.onChangeHandler(event, 'image')}
+            ></input>{'   '}
+          <br />
+          <button>Save Item</button>
+        </form>
         
           {this.props.store.shelf.setShelf.map((item) => (
             <InfoItem key={item.id} item={item} />
